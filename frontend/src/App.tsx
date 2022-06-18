10f6f6bb9f1aa31a15f6c886c1d6a7d141e01147
Sandu <sandufurdui@gmail.com>
2022-06-18 18:49:37 +0300

    small frontend changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import QrScanner from './QrScanner';
 
 function App() {
-  const [data, setData] = useState('No result');
+  const [data, setData] = useState<string>('No result');
+  const [shown , setShown] = useState(false);
+
   return (
-    <div className="App">
+    <div className="App bg-blue">
       <h1>
         Hello world!
       </h1>
-      <QrScanner />
+      <button onClick={()=>{setShown(prev => !prev)}}>Qr mode</button>
+      {shown? <QrScanner /> : null}
     </div>
   );
 }
