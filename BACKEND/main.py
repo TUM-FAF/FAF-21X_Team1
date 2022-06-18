ce415febe010c45c91b6f66010fa000c4d096792
Smocvin Denis <denis.smocvin@isa.utm.md>
2022-06-18 18:17:24 +0300

    feat: implement qr and check routes

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -3,23 +3,22 @@ from flask import Flask
 from flask import Flask, Response
 from flask import jsonify, request
 import json
+import random
 
 app = Flask(__name__)
 
 users = []
 
-question = 'How are you ?'
+f = open('../questions.json')
+questions = json.load(f)
+f.close()
+
+POINTS_THRESHOLD = 12
 
 class User:
     def __init__(self, name):
         self.name = name
-        self.points = 0
-        # users.append(self)
-
-# class Hunt:
-#     def __init__(self, voucher):
-#         self.voucher = voucher
-#         self.participants = []
+        self.points = 0     
 
 def reset():
     global users
@@ -28,32 +27,34 @@ def reset():
 
 # body: name
 # response: questions
-@app.route('/qr/<string:name>', methods=['POST'])
-def qr(name):
-    if request.method == 'POST':
-        user = User(name)
-        users.append(user)
+@app.route('/qr/<int:qr_id>/<string:name>', methods=['POST'])
+def qr(qr_id, name):
+    global users
 
-        return jsonify({"question": question})
+    user = User(name)
+    users.append(user)
 
-# query: username
+    question = questions[qr_id - 1]['question']
+
+    return jsonify({"question": question})
+
+
 # body: answer (answer from the user to a single question)
 # response: points (points accumulated)
-@app.route('/check/<string:name>', methods=['GET'])
-def check(name):
-    answer = request.json['answer']
+@app.route('/check/<int:qr_id>/<string:name>', methods=['POST'])
+def check(qr_id, name):
+    answers = questions[qr_id - 1]['answers']
+    user_answer = json.loads(request.data)['answer']
+    points = 0
+
+    if user_answer in [answer['variant'] for answer in answers]:
+        points = [answer['score'] for answer in answers if answer['variant'] == user_answer][0]
     
+    user = [u for u in users if u.name == name][0]
+    user.points = points
 
-# query: username
-# response: object (message: win/lost, voucher)
-@app.route('/result/<string:name>', methods=['GET'])
-def result():
-    pass
+    return jsonify({'answer': user_answer, 'points': points})
 
-# @app.route('/insurance', methods = ['GET'])
-# def type_of_insurance():
-#     if request.method == 'GET':
-#         return jsonify({'result': 'good'})
 
 if __name__ == '__main__':
      app.run()
